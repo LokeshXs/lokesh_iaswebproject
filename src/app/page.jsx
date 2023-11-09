17f98042bb78068a3f8626754c54bf7e0456294e
LOKESH SINGH <lokesh1129ece@gmail.com>
2023-11-09 11:03:02 +0530

    initial comming, adding the files

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <h1 className="text-8xl font-semibold max-lg:text-6xl max-sm:text-4xl">Hello, <span className="bg-black text-white p-4 rounded-xl">Truly IAS</span></h1>
+    </main>
+  )
+}
